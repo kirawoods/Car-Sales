fac0f5eb5a0867f6d6d5cc2af205030c81f602a9
Kira <kawoods826@gmail.com>
2020-01-21 10:22:48 -0800

    completed MVP

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { removeFeature } from "../actions";
 
 const AddedFeature = props => {
   return (
@@ -22,4 +23,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps, {})(AddedFeature);
+export default connect(mapStateToProps, { removeFeature })(AddedFeature);
